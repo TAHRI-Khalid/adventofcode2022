d18c006ea7f0df6e9cddd90527f136cc192e8b3a
TAHRI Khalid <tahri.tk@gmail.com>
2022-12-11 14:02:44 +0100

    day 11, 1 pazzle

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,7 +1,5 @@
 
 =
-data/day2.txt,c/b/cb62a82369027ad0cd9e727ed9d4a9f206c5c3cf
-=
 data/day3.txt,9/c/9c350795f780412e3a4846755ea4e131b42d9594
 F
 src/tk/tahri/Day4.java,5/0/509c8d33d16a6c1cae0a9d8ebc6e9ca24da69900
@@ -15,3 +13,29 @@ F
 data/day7.txt,c/5/c5529b0129e77b55db87da156283652897f59e97
 F
 src/tk/tahri/Day5.java,0/4/04204385a0bbb0c66211d97c33f3a9aad0f64711
+F
+src/tk/tahri/Day3.java,4/5/459ed726eed52ea64f5ec8fe276c431763c87cf2
+@
+adventofcode.iml,3/2/326b887bdcec760949bc5ffcd24838d812b25a40
+F
+src/tk/tahri/Day7.java,e/2/e2f89670b52181bd2c0b18d07a9a9c450b20f55b
+=
+data/day8.txt,1/c/1cceaa0749d143b59e82ccbfe37895e8b9699ca7
+F
+src/tk/tahri/Day9.java,4/c/4c7079dd7e4e20f5f57ca2282efe6a2b5f76e3ac
+=
+data/day9.txt,9/4/94a1fad10486c930ab44b40d43dd28faba8f1862
+=
+data/day1.txt,b/b/bb1d96825cfa792ab7fd924a19b8616cba235ae4
+G
+src/tk/tahri/Day10.java,f/7/f7dd71f863feb14c6e5c0e11fe49dbb7bb234dc0
+F
+src/tk/tahri/Day1.java,8/7/872b29a4908bf01c3824d6011118ff4d330b51b2
+F
+src/tk/tahri/Day2.java,c/b/cb137081d89f3a680e25324beb299d896d4fc940
+=
+data/day2.txt,c/b/cb62a82369027ad0cd9e727ed9d4a9f206c5c3cf
+>
+data/day10.txt,c/5/c52844b69b76aa955efa13297fc01076338c3e05
+F
+src/tk/tahri/Day8.java,1/9/195b9cf1a78d8194415e9d674cdeea3f639de3ef
---
+++ src/tk/tahri/Day11.java
@@ -0,0 +1,95 @@
+package tk.tahri;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Day11 {
+    public static final Path PATH = Path.of("data/day11.txt");
+
+    public static void main(String[] args) throws IOException {
+
+        var monkeys = Arrays.stream(Files.readString(PATH).split("\n\n"))
+                .map(Monkey::new).toList();
+
+        for (int round = 0; round < 20; round++) {
+            for (int i = 0; i < monkeys.size(); i++) {
+                var monkey = monkeys.get(i);
+                monkey.countInspectedItems();
+
+                for (int j = 0; j < monkey.items.size(); j++) {
+                    var item = monkey.items.get(j);
+                    item = monkey.applyWorryLevel(item);
+                    item = Math.floorDiv(item, 3);
+
+                    if (item % monkey.divisibleBy == 0) {
+                        monkeys.get(monkey.testIsTrueThrowToMonkey).items.add(item);
+                    } else {
+                        monkeys.get(monkey.testIsFalseThrowToMonkey).items.add(item);
+                    }
+                }
+
+                monkey.items.clear();
+            }
+            System.out.println("-------------------Round "+ round);
+            monkeys.forEach(monkey -> System.out.println(monkey.items));
+
+        }
+        monkeys.forEach(monkey -> System.out.println(monkey.inspectedItems));
+
+        var level = monkeys.stream()
+                .map(value -> value.inspectedItems)
+                .sorted(Comparator.reverseOrder())
+                .limit(2)
+                .toList();
+
+        System.out.println(level);
+        System.out.println(level.get(0) * level.get(1));
+    }
+}
+
+class Monkey {
+
+    public List<Long> items;
+    private String op;
+    public final int divisibleBy;
+    public final int testIsTrueThrowToMonkey;
+    public final int testIsFalseThrowToMonkey;
+    public int inspectedItems = 0;
+
+    public Monkey(String note) {
+        var obs = note.split("\n");
+        items = Arrays.stream(obs[1].replace("Starting items: ", "")
+                        .split(","))
+                .map(String::trim)
+                .map(Long::parseLong)
+                .collect(Collectors.toList());
+
+        op = obs[2].replace("Operation: new = old ", "").trim();
+        divisibleBy = Integer.parseInt(obs[3].replace("Test: divisible by", "").trim());
+        testIsTrueThrowToMonkey = Integer.parseInt(obs[4].replace("If true: throw to monkey ", "").trim());
+        testIsFalseThrowToMonkey = Integer.parseInt(obs[5].replace("If false: throw to monkey ", "").trim());
+
+    }
+
+    public long applyWorryLevel(long numbre) {
+        var ops = op.split(" ");
+        if (ops[0].equals("*")) {
+            if (ops[1].equals("old")) {
+                return numbre * numbre;
+            }
+            return numbre * Integer.parseInt(ops[1]);
+        } else {
+            return numbre + Integer.parseInt(ops[1]);
+        }
+    }
+
+    public void countInspectedItems() {
+        this.inspectedItems += items.size();
+    }
+
+}
